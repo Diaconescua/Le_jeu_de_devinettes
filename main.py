d79092a86f517059520dfcaac1f06c391a23c156
diaconescua <diaconescua@educ.college-montreal.qc.ca>
2023-10-13 11:37:46 -0400

    e

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import random
+
 # je definer une fonction qui contient le jeu de devinette dans lequel le usager mets un nombre les bornes minimal et maximale, puis l'ordinateur choisi un nombre random et l'usgaer essaye de le devinir et l'ordinateur l'aide en le disant si c'est plus grand ou plus petit.
-
-
 def jeu_de_devinettes():
   rejouer = "o"
 
   #Cet boucle while execute le jeux jusqu'a le usager veut plus jouer
-
   while rejouer == "o":
     borne_min = int(input("définisser les bornes, nombre minimal:"))
     borne_max = int(input("définisser les bornes, nombre maximal:"))
@@ -14,24 +12,20 @@ def jeu_de_devinettes():
     essai = 0
     guess = None
 
-    #L’ordinateur demande à l’usager d’entrer un nombre de façon à trouver celui que l'ordi a choisi.
-
-    print(
-        f"J'ai choisi un nombre au hasard entre {borne_min} et {borne_max}"
-    )
+    print(f"J'ai choisi un nombre au hasard entre {borne_min} et {borne_max}")
     print("À vous de le deviner...")
 
+    # L’ordinateur demande à l’usager d’entrer un nombre de façon à trouver celui que l'ordi a choisi
+    # et l'ordinateur l'aide en le disant si c'est plus grand ou plus petit.Aussi, il demande a l'usage s'il veut rejouer
     while n != guess:
       guess = int(input("Entrer un nombre: "))
       essai += 1
       if guess > n:
         print("la reponse est plus petite que votre nombre!")
-
       elif guess < n:
         print("la reponse est plus grande que votre nombre!")
 
-    print("C'est la bonne reponse, vous avez gagnez!!!\nCela vous a pris",
-          essai, "essai(s).")
+    print("C'est la bonne reponse, vous avez gagnez!!!\nCela vous a pris",essai, "essai(s).")
 
     rejouer = input("Voulez-vous faire une autre partie (o/n)? ").lower()
 
