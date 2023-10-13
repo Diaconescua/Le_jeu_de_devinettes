8ddbba940994cad0bb7073d9a9f1ba7be1ac896c
diaconescua <diaconescua@educ.college-montreal.qc.ca>
2023-10-13 11:32:02 -0400

    n

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
+import random
 # je definer une fonction qui contient le jeu de devinette dans lequel le usager mets un nombre les bornes minimal et maximale, puis l'ordinateur choisi un nombre random et l'usgaer essaye de le devinir et l'ordinateur l'aide en le disant si c'est plus grand ou plus petit.
 
 
 def jeu_de_devinettes():
-  import random
   rejouer = "o"
 
   #Cet boucle while execute le jeux jusqu'a le usager veut plus jouer
 
   while rejouer == "o":
-    borne_minimale = int(input("définisser les bornes, nombre minimal:"))
-    borne_maximale = int(input("définisser les bornes, nombre maximal:"))
-    n = random.randrange(borne_minimale, borne_maximale)
+    borne_min = int(input("définisser les bornes, nombre minimal:"))
+    borne_max = int(input("définisser les bornes, nombre maximal:"))
+    n = random.randint(borne_min, borne_max)
     essai = 0
     guess = None
 
     #L’ordinateur demande à l’usager d’entrer un nombre de façon à trouver celui que l'ordi a choisi.
 
     print(
-        f"J'ai choisi un nombre au hasard entre {borne_minimale} et {borne_maximale}"
+        f"J'ai choisi un nombre au hasard entre {borne_min} et {borne_max}"
     )
     print("À vous de le deviner...")
 
